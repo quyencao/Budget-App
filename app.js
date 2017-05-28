f9dce63ab4bb222b15efc178cb7be600f73890bb
quyencao <boy20195@gmail.com>
2017-05-29 01:14:09 +0700

    Read value from input and make DOM strings in one place

--- app.js
+++ app.js
@@ -11,9 +11,25 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
     
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
     
-    // Some Code
-    
+    return {
+        getInput: function() {
+            return {
+                type : document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        getDOMStrings: function() {
+            return DOMstrings;
+        }
+    };
     
 })();
 
@@ -21,8 +37,12 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
     
+    var DOM = UICtrl.getDOMStrings();
+    
     var ctrlAddItem = function() {
         // 1. Get the field input data
+        var input = UICtrl.getInput(); 
+        console.log(input);
         
         // 2. Add the items to budget controller
         
@@ -31,11 +51,9 @@ var controller = (function(budgetCtrl, UICtrl){
         // 4. Caculate the budget
         
         // 5. Display the budget in the UI
-        
-        console.log('It works');
     }
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keypress', function(event) {
         
