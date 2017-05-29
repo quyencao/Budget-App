bc853b14e7c771dab8d1b82b49bcaedb8ca0db4c
quyencao <boy20195@gmail.com>
2017-05-30 01:18:55 +0700

    Clear fields + Convert NodeList to Array + Set focus + forEach

--- app.js
+++ app.js
@@ -93,6 +93,25 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
         
+        clearFields: function() {
+             var fields, fieldsArr; 
+             
+             // Get all input element    
+             fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+              
+             // Convert NodeList to Array(can't call on fields because it not an array)
+             // Set this to fields NodeList
+             fieldsArr = Array.prototype.slice.call(fields);  
+            
+             // Clear all fields
+             fieldsArr.forEach(function(current, index, array) {
+                  current.value = '';
+             });
+                
+             // Set focus back to description field
+             fieldsArr[0].focus();
+        },
+        
         getDOMStrings: function() {
             return DOMstrings;
         }
@@ -129,9 +148,13 @@ var controller = (function(budgetCtrl, UICtrl){
     
         // 3. Add new item to the UI
         UICtrl.addListItem(newItem, input.type);
-        // 4. Caculate the budget
         
-        // 5. Display the budget in the UI
+        // 4. Clear the fields
+        UICtrl.clearFields();
+        
+        // 5. Caculate the budget
+        
+        // 6. Display the budget in the UI
     }
     
     return {
